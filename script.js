b58d90191133ddcff59093d8a1f3f95523cf76e7
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-20 12:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Write your script here
+
+document.addEventListener("DOMContentLoaded", function () {
+  const div = document.createElement("div");
+  div.id = "browser-info";
+  div.innerText = `You are using ${navigator.appName} version ${navigator.appVersion}`;
+  document.body.insertBefore(div, document.body.firstChild);
+});
